72a85cffb7da409b09c5241805b60b6869b14d94
krzychu598 <wnorowski.krzysztof98@gmail.com>
2024-05-21 19:35:37 +0200

    Test configuration v3

---
+++ WarehouseTests/test.cpp
@@ -0,0 +1,9 @@
+#include "pch.h"
+#include "../WarehouseProject/Warehouse.cpp"
+
+//before tunning tests please install required package specified in packages.config
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
