9c830e342915c4dfa8b4e13e8e4fd6e28e8f59dd
krzychu598 <wnorowski.krzysztof98@gmail.com>
2024-05-22 14:33:55 +0200

    Configure tests, gitignore

--- WarehouseTests/test.cpp
+++ WarehouseTests/test.cpp
@@ -1,7 +1,14 @@
 #include "pch.h"
+#include "../WarehouseProject/StorageSpace.cpp"
 #include "../WarehouseProject/Warehouse.cpp"
+#include "../WarehouseProject/Area.cpp"
+#include "../WarehouseProject/Shelving.cpp"
+#include "../WarehouseProject/Box.cpp"
+#include "../WarehouseProject/Employee.cpp"
+#include "../WarehouseProject/Product.cpp"
 
-//before tunning tests please install required package specified in packages.config
+
+//before running tests please install required package specified in packages.config
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
