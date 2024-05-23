683ec8c7ac981e4485626cd47e7045474697b7dc
krzychu598 <wnorowski.krzysztof98@gmail.com>
2024-05-23 23:42:11 +0200

    revert

--- WarehouseProject/Warehouse.h
+++ WarehouseProject/Warehouse.h
@@ -6,19 +6,16 @@
 #include <string>
 #include <stdexcept>
 #include <fstream>
-#include "packages/nlohmann.json.3.11.2/build/native/include/nlohmann/json.hpp"
+#include "include/nlohmann/json.hpp"
 
 class Warehouse :
     public StorageSpace
 {
 public:
     Warehouse(const std::string& file_path);
-    ~Warehouse();
-    void put(std::string name, std::string type);
-    bool find(std::string name, std::string type) const;
+    void put(std::string name);
+    std::vector<std::string> products;
 private:
-
     std::vector<std::unique_ptr<Area>> areas;
-    std::string name;
 };
 
