1e263ac3344b4a005cc4bafced1ec4070bc533d2
krzychu598 <wnorowski.krzysztof98@gmail.com>
2024-05-25 22:04:04 +0200

    Reset 4

---
+++ WarehouseProject/WarehouseApp/WarehouseMain.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include "Warehouse.h"
+
+// Zmienne z _ np. storage_space
+// Funkcje camelCase
+// Push to dev
+// Podział StorageSpace po konsultacji ze wszystkimi
+// Warehouse, Area - Krzychu; Shelving, Box - Maks; Product, Employee - Paweł
+int main() {
+
+	//experimental code sample
+	Warehouse warehouse("../WarehouseApp/warehouse.json");
+	warehouse.put("Foo", "Electronics");
+	if (warehouse.find("Foo", "Electronics") == true) {
+		std::cout << "object found\n";
+	}
+	else {
+		std::cout << "object not found\n";
+	};
+	return 0;
+
+}
