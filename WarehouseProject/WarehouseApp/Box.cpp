55d70b884b60f13e8dad7a6a4e7a064d719967b0
Max <01178617@pw.edu.pl>
2024-05-29 21:09:38 +0200

    Added method and class variable declarations, filling out the definitions after consultation

--- WarehouseProject/WarehouseApp/Box.cpp
+++ WarehouseProject/WarehouseApp/Box.cpp
@@ -1,1 +1,8 @@
 #include "Box.h"
+
+Box::Box(const nlohmann::json& box) {};
+void Box::put(Product& product) {};
+unsigned int Box::getProductAmount() const {};
+unsigned int Box::getEmptySpace() const {};
+unsigned int Box::getOccupiedSpace() const {};
+void Box::updateOccupiedSpace() {};
--- WarehouseProject/WarehouseApp/Box.h
+++ WarehouseProject/WarehouseApp/Box.h
@@ -1,7 +1,23 @@
 #pragma once
 #include "StorageSpace.h"
+#include "nlohmann/json.hpp"
+#include "Product.h"
+
 class Box :
     public StorageSpace
 {
+public:
+    Box(const nlohmann::json& box);
+    void put(Product& product);
+    unsigned int getProductAmount() const;
+    unsigned int getEmptySpace() const;
+    unsigned int getOccupiedSpace() const;
+    void updateOccupiedSpace();
+private:
+    std::vector<std::unique_ptr<Product>> products;
+    std::string product;
+    unsigned int size = 360;
+    unsigned int occupied_space_size;
+    std::string type;
 };
 
